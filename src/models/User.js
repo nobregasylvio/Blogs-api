6244ccfa93e1c90bf3374d858e7284c5fd2652a3
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2022-12-24 13:27:32 -0300

    Req02 - Criando table User

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('User', {
     id: DataTypes.INTEGER,
-    display_name: DataTypes.STRING,
+    displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING
