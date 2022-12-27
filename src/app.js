607201e8b1d0f61e0552e46b131d31d4d364d1aa
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2022-12-27 19:06:54 -0300

    Criando todas as rotas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const categoriesRoute = require('./routes/categories.routes');
+const loginRoute = require('./routes/login.routes');
+const postRoute = require('./routes/post.routes');
+const userRoute = require('./routes/user.routes');
 
 // ...
 
@@ -7,6 +11,11 @@ const app = express();
 app.use(express.json());
 
 // ...
+
+app.use('/login', loginRoute);
+app.use('/user', userRoute);
+app.use('/categories', categoriesRoute);
+app.use('/post', postRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
